27705bdc62c2d6c72a86742a5afdd9c7e5bc78af
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-07-02 10:46:45 -0500

    agregamos fecha

--- javascrip.js
+++ javascrip.js
@@ -3,3 +3,32 @@ function buscarPoID(idElemento){
  
  return  elementoDocument;
 }
+
+function fecha(){
+    let elementoFecha = document.getElementById('fecha');
+    let fechaActual = new Date()
+    let dia = fechaActual.getDate();
+    let mes = fechaActual.getMonth() + 1;
+    let año = fechaActual.getFullYear();
+
+    let fechaYhoraActualizada = ('Fecha actualizada: '+ dia + '/' + mes + '/' + año)  
+    elementoFecha.textContent = fechaYhoraActualizada
+}
+
+let miArray = [1, 7, 4, 8, 9, 5, 6];
+function btnCalificacionMasAlta(){
+    let btnElemento = buscarPoID('calificacionMasAlta')
+    btnElemento.addEventListener('click', function(){
+        for(let numero of miArray){
+            if(numero <= )
+        }
+    })
+}
+
+
+
+
+
+
+
+fecha()
