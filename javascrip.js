3fe8ad2ef687f83c1ed7dbac6e5e297dd65eccbc
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-07-05 15:35:22 -0500

    terminamos la aplicacion del boletin estudiantil cuenta con 4 botones que nos permiten saber mas a detalle sobre las notas del estudiante

--- javascrip.js
+++ javascrip.js
@@ -15,7 +15,7 @@ function fecha(){
     elementoFecha.textContent = fechaYhoraActualizada
 }
 
-let miArray = [1, 7, 4, 8, 9, 5, 6];
+let miArray = [10, 7, 4, 8, 9, 5, 6, 2];
 function btnCalificacionMasAlta(){
     let btnElemento = buscarPoID('calificacionMasAlta');
     let inputResultado = buscarPoID('inputResultado');
@@ -57,11 +57,37 @@ function btnObtenerTodasCalificaciones(){
   elementoBtn.addEventListener('click', function(){
     let mensaje = ''
     for(let i = 0; i < miArray.length; i++){
-      mensaje += miArray[i] + ',';
+      mensaje += miArray[i] +  '-' ;
       /* mensaje = mensaje + ' ' + miArray[i] + ' ,'; */
     } 
     inputResultado.value = mensaje.substring(0, mensaje.length-1);
     console.log(mensaje.substring(0, mensaje.length-1))
+  })
+}
+
+function btnObtenerAplazo(){
+  let elementoIdBtn = buscarPoID('btnAplazo');
+  let cajaInputResultado = buscarPoID ('inputResultado');
+
+  elementoIdBtn.addEventListener('click', function(){
+    let i = 0
+    let numero;
+    let mensaje= '';
+    do {
+      numero = miArray[i++];
+     /*  if(numero < 5){
+        console.log(numero)
+      } */
+      if(numero >= 5){
+        continue /* Esta forma en la que utilizamos el continue, es una forma muy poco funcional, 
+        aqui la usamos para poder reforzar lo que estamos aprendiendo. la forma correcta es la que esta comentada arriba, 
+        que seria con el dondicional unicamente. */
+      }
+      mensaje += numero + '-'
+      console.log(numero)
+      cajaInputResultado.value = mensaje.substring(0, mensaje.length-1)
+
+    } while(i < miArray.length)
   })
 }
 
@@ -70,3 +96,4 @@ fecha();
 btnCalificacionMasAlta();
 btnPromedio(miArray);
 btnObtenerTodasCalificaciones();
+btnObtenerAplazo();
