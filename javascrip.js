0e0cf2d52c774c68256d8965a44779e186ce7596
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-07-04 19:44:04 -0500

    agregamos una nueva funcion a un nuevo btn

--- javascrip.js
+++ javascrip.js
@@ -17,18 +17,56 @@ function fecha(){
 
 let miArray = [1, 7, 4, 8, 9, 5, 6];
 function btnCalificacionMasAlta(){
-    let btnElemento = buscarPoID('calificacionMasAlta')
+    let btnElemento = buscarPoID('calificacionMasAlta');
+    let inputResultado = buscarPoID('inputResultado');
+    numeroMax = miArray[0]
+    let i = 0;
     btnElemento.addEventListener('click', function(){
-        for(let numero of miArray){
-            if(numero <= )
+        while ( i < miArray.length){
+          if (miArray[i] > numeroMax) {
+            numeroMax = miArray[i];
+          }
+          i++;
         }
+        inputResultado.value = numeroMax;
     })
 }
 
 
+function btnPromedio(arr) {
+    let btnPromedio = buscarPoID('btnPromedio');
+    let inputResultado = buscarPoID('inputResultado');
+  
+    btnPromedio.addEventListener('click', function() {
+      let suma = 0;
+      for (let i = 0; i < arr.length; i++) {
+        suma += arr[i];
+      }
+      let promedio = suma / arr.length;
+  
+      inputResultado.value = promedio;
+    });
+}
 
 
+function btnObtenerTodasCalificaciones(){
+  let elementoBtn = buscarPoID('todasCalificaciones');
+  let inputResultado = buscarPoID('inputResultado');
+  
+
+  elementoBtn.addEventListener('click', function(){
+    let mensaje = ''
+    for(let i = 0; i < miArray.length; i++){
+      mensaje += miArray[i] + ',';
+      /* mensaje = mensaje + ' ' + miArray[i] + ' ,'; */
+    } 
+    inputResultado.value = mensaje.substring(0, mensaje.length-1);
+    console.log(mensaje.substring(0, mensaje.length-1))
+  })
+}
 
 
-
-fecha()
+fecha();
+btnCalificacionMasAlta();
+btnPromedio(miArray);
+btnObtenerTodasCalificaciones();
